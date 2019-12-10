c6fed78a62de3f3a3752919849240606546ae1dc
robsztal <r.stoklosa00@gmail.com>
2019-12-10 18:54:12 +0100

    def add
    dodawanie elementów

--- Models/BloodElement.py
+++ Models/BloodElement.py
@@ -1,10 +1,10 @@
 class BloodElement:
 
-    def __init__(self, name: str, unit: str, minValue: float, maxValue: float):
+    def __init__(self, name: str, unit: str, min_value: float, max_value: float,):
         self.name = name
         self.unit = unit
-        self.minValue = minValue
-        self.maxValue = maxValue
+        self.min_value = min_value
+        self.max_value = max_value
 
     name: str
     unit: str
--- Repositories/BloodElementsRepository.py
+++ Repositories/BloodElementsRepository.py
@@ -1,11 +1,50 @@
 from Models.BloodElement import BloodElement
+import json
+
 
 class BloodElementsRepository:
-    def add(self, element: BloodElement) -> BloodElement:
-        raise NotImplementedError
+
+    def contains(self, name: str):
+
+        elements = json.load(open("elements.json", "r"))
+        for element in elements:
+            if element["name"] == name:
+                return True
+            """elif element["unit"] == unit:
+                return True
+            elif element["max_value"] == max_value:
+                return True
+            elif element["min_value"] == min_value:
+                return True"""
+        return False
+
+    def add(self, element):
+
+        if self.contains(element.name):
+            return False
+
+        elements_file = open("elements.json", "r")
+        elements = json.load(elements_file)
+        elements_file.close()
+        elements.append(element.__dict__)
+        elements_file = open("elements.json", "w")
+        json.dump(elements, elements_file)
+        elements_file.close()
+        return True
 
     def get(self, name : str) -> BloodElement:
         raise NotImplementedError
 
     def delete(self, name: str):
         raise NotImplementedError
+
+
+newElement = BloodElement("name", "unit", 0, 0)
+newElement.name = "krwinka"
+newElement.unit = "mg/l"
+newElement.max_value = 3
+
+repository = BloodElementsRepository()
+
+repository.add(newElement)
+print(repository.contains("krwinka"))
